31d3b46073fa3fb4a58e697c115da4f92da7a946
Menaid <menaidnocic@live.se>
2023-09-26 13:36:55 +0200

    First commit

---
+++ Proffesion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace terminal_game
+{
+    internal class Proffesion
+    {
+    }
+}
